7769d4e71bf3941694f5a9c3cd95f8c37ebe9544
Ehsanul Alam Sabbir <easabbir@gmail.com>
2025-08-19 23:57:43 +0600

    added github actions

--- tests/test1.js
+++ tests/test1.js
@@ -1,4 +1,5 @@
-fixture('This is my testcafe tests').page('https://www.google.com/');
+fixture('This is my testcafe tests')
+    .page('https://www.google.com/');
 
 test('This is my 1st test', async t => {
     await t.maximizeWindow()
@@ -11,16 +12,12 @@ test('This is my 1st test', async t => {
 test('This is my 2nd test', async t => {
     await t
         .maximizeWindow()
-        .typeText('#APjFqb','Learning TestCafe test 2 nd test')
+        .typeText('#APjFqb','Learning TestCafe')
         .click('.gNO89b')
         .wait(2000);
 });
 
-test('This is my 3rd test', async t => {
-    await t
-        .maximizeWindow()
-        .typeText('.gLFyf','sabbir')
-        .click('.gNO89b')
-        .wait(2000)
-});
-
+//fixture: is a keyword.
+//page: is a method to set the page URL where the test will run.
+// TestCafe tests folder consists of fixtures and tests.
+// A fixture is groups of tests that share the same starting URL.
--- tests/test3.js
+++ tests/test3.js
@@ -4,18 +4,12 @@ import { Selector } from "testcafe";
 fixture ('Getting Started for test3.js file')
 .page('https://trytestingthis.netlify.app/'); 
 
-// test('3rd test', async t => {
-//     await t
-//         .maximizeWindow()
-//         .typeText('#fname','Ehsanul Alam')
-//         .typeText('#lname', 'sabbir')
-    
-
-// });
-
-test('4th test', async t => {
+test('3rd test', async t => {
     await t
         .maximizeWindow()
-        .typeText(Selector('input').withText('Text'), 'Ehsanul Alam sabbir');
-        
-})
+        .typeText('#fname','Ehsanul Alam')
+        .typeText('#lname', 'sabbir')
+    
+
+});
+
